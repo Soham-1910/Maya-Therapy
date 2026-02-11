4c8e8ba98dd7c36015acad661320b36560725d14
Soham-1910 <sohamgurav000@gmail.com>
2026-02-11 09:16:59 +0530

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,18 +5,9 @@ import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        suppressHydrationWarning
-        className={`${gopher.variable} bg-[#DCD1E6] font-sans antialiased`}
-      >
-        <Navbar /> {/* Only here */}
+    <html lang="en" className={gopher.variable}>
+      <body className="bg-[#DCD1E6] antialiased">
+        <Navbar />
         <main>{children}</main>
         <Footer />
       </body>
